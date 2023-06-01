5d3e9281ae71d18ec69d629a7abd18e096c839bd
amirhmh3 <seeonline99@gmail.com>
2023-06-01 15:36:52 -0700

    feat : check singel responce in BaseResponse and register Colleague

--- app/Http/Controllers/api/ColleagueController.php
+++ app/Http/Controllers/api/ColleagueController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers\api;
 
+use App\Base\BaseResponse;
 use App\Http\Controllers\BaseController;
 use App\Http\repository\ColleagueRepository;
-use App\Http\repository\Repository;
-use App\Http\Service\Service;
+use App\Http\Service\ColleagueService;
 use Illuminate\Http\Request;
 
 class ColleagueController extends BaseController
 {
     public $service;
     public $repository;
-    public function __construct(Request $request, Service $service,ColleagueRepository $repository)
+    public function __construct(Request $request, ColleagueService $service,ColleagueRepository $repository)
     {
         parent::__construct($request, $service,$repository);
         $service->setRepository($repository);
@@ -21,5 +21,11 @@ class ColleagueController extends BaseController
 
     }
 
+    public function store(Request $request)
+    {
+        $param=$request->all();
+        $result=$this->service->store($param);
+        return BaseResponse::JSON(true,$result,201);
+    }
 
 }
--- app/Http/Service/ColleagueService.php
+++ app/Http/Service/ColleagueService.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Service;
 
+use App\Base\BaseResponse;
+use Illuminate\Support\Facades\Validator;
+
 class ColleagueService extends Service
 {
+    public function store($param)
+    {
+        $roll = [
+            'user_id' => ['required',"integer"],
+            'name' => ['required'],
+            'family' => ['required'],
+            'status' => ['required']
+        ];
+        $validate = Validator::make($param, $roll);
+        if ($validate->fails()) {
+            return BaseResponse::JSON( false,  $validate->errors(), 401);
+        }
 
+        return $this->repository->store($param);
+
+    }
 }
