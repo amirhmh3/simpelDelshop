526052a8f33c2eebe7b6000349fe40221dda6070
amirhmh3 <seeonline99@gmail.com>
2023-06-01 01:15:45 -0700

    feat : init pasport

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register',[\App\Http\Controllers\api\Auth\UserController::class,'register']);
+Route::post('login',[\App\Http\Controllers\api\Auth\UserController::class,"login"]);
+
