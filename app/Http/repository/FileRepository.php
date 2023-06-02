7ad1d9b6351629e8c34038945453daa1c998e80b
amirhmh3 <seeonline99@gmail.com>
2023-06-02 12:21:01 -0700

    fix : change route save file

--- app/Http/repository/FileRepository.php
+++ app/Http/repository/FileRepository.php
@@ -3,6 +3,7 @@
 namespace App\Http\repository;
 
 use App\Models\File;
+use Illuminate\Support\Str;
 
 class FileRepository extends Repository
 {
@@ -15,11 +16,13 @@ class FileRepository extends Repository
 
     public function store($param)
     {
-        $path=$param->file('file')->store('file');
+        $type_file=$param->file('file')->extension();
+        $file_name=Str::random(40).".".$type_file;
+        $param->file('file')->storeAs('public/file',$file_name);
         $data=[
             "customer_id"=>$param["user_id"],
             "post_id"=>$param["post_id"],
-            "url"=>"file:///E:/project/laravel/simpelDelshop/storage/app/".$path,
+            "url"=>asset("storage/file/".$file_name),
             "file_type_id"=>$param["file_type_id"]
         ];
 
