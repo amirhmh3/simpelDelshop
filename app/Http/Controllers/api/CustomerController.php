d83c10102a8c3c0f48acd2afa3de0c9b854e53c8
amirhmh3 <seeonline99@gmail.com>
2023-06-02 02:36:26 -0700

    feat : base file and rout group controller

---
+++ app/Http/Controllers/api/CustomerController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Base\BaseResponse;
+use App\Http\Controllers\BaseController;
+use App\Http\repository\ColleagueRepository;
+use App\Http\repository\CustomerRepository;
+use App\Http\Service\ColleagueService;
+use App\Http\Service\CustomerService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CustomerController extends BaseController
+{
+    public $service;
+    public $repository;
+    public function __construct(Request $request, CustomerService $service,CustomerRepository $repository)
+    {
+        parent::__construct($request, $service,$repository);
+        $service->setRepository($repository);
+        $this->service=$service;
+        $this->repository=$repository;
+
+    }
+
+}
