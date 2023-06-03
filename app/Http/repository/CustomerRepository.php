56d75641c1f064dcc80135bf6c28f3102e02c2c4
amirhmh3 <seeonline99@gmail.com>
2023-06-03 05:21:57 -0700

    fix chanck status register_form in user

--- app/Http/repository/CustomerRepository.php
+++ app/Http/repository/CustomerRepository.php
@@ -3,6 +3,7 @@
 namespace App\Http\repository;
 
 use App\Models\Customer;
+use App\Models\User;
 
 class CustomerRepository extends Repository
 {
@@ -11,6 +12,14 @@ class CustomerRepository extends Repository
     {
         parent::__construct($model);
         $this->model=$model;
+    }
+
+    public function store($param)
+    {
+        $result=$this->model->create($param);
+        $user=User::find($param["user_id"]);
+        $user->update(["register_form"=>true]);
+        return $result;
     }
 
 }
